6a9182f3abdd9040de9c2b05a7b941273b799794
yinchaodear <739822061@qq.com>
2020-12-11 15:25:08 +0800

    修改各个页面
    1

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,4 +1,5 @@
 const app = getApp()
+const router = require('../../utils/router')
 let _this;
 Page({
 
@@ -7,15 +8,22 @@ Page({
    */
   data: {
     phone:'',
-    dphone:''
+    password:''
   },
+
+
+  navTo(e) {
+    var path = e.currentTarget.dataset.path;
+    router.navigateTo(path);
+  },
+
   ddinput(e){
+  
     let name = e.currentTarget.dataset.name;
     this.data[name] = e.detail.value;
-    
     this.setData({
       phone: this.data.phone,
-      dphone: this.data.dphone
+      password: this.data.password
     })
   },
   /**
@@ -41,6 +49,11 @@ Page({
         title: '请填写手机号',
         icon:'none'
       })
+    }else   if(this.data.password == ''){
+      wx.showToast({
+        title: '请填写密码',
+        icon:'none'
+      })
     }else{
       wx.showLoading({
         title: '授权中',
@@ -50,33 +63,7 @@ Page({
       userInfo.id = wx.getStorageSync("user").id
       userInfo.phone = this.data.phone
       userInfo.dphone = this.data.dphone
-      app.com.post('wx/user/update', userInfo, function (res) {
-        wx.hideLoading()
-        if(res.code == 1){
-          wx.showToast({
-            title: '授权成功',
-            mask:true,
-            duration:800
-          })
-          let user = res.data
-          user.phone = userInfo.phone
-          wx.setStorage({
-            key: 'user',
-            data: user,
-          })
-          // wx.setStorageSync("user", user)
-          setTimeout(function(){
-            wx.navigateBack({
-              detal:1
-            })
-          },900)
-        }else{
-          wx.showToast({
-            title: '授权失败',
-            icon:'none'
-          })
-        }
-      })
+      
     }
     
   },
--- pages/register/register.js
+++ pages/register/register.js
@@ -12,6 +12,18 @@ Page({
     show:true,
     isUpdate:false
   },
+
+
+   complete(e){
+     debugger
+    console.log(e);
+    var type = e.currentTarget.dataset.type;
+    var value =e.detail.value;
+    this.setData({
+      [type]:value
+    })
+
+  },
   gotoUpdate(){
     this.setData({
       show:true,
@@ -79,68 +91,8 @@ Page({
     }
   },
   
-  formSubmit(e) {
-    let formId = e.detail.formId
-    if (e.detail.value.name == '') {
-      wx.showToast({
-        title: '请输入填姓名',
-        icon: 'none'
-      })
-    } else if (e.detail.value.card_num == ''){
-      wx.showToast({
-        title: '请输入身份证号码',
-        icon: 'none'
-      })
-    }else if (this.data.stu_card == '' || this.data.stu_card.indexOf('tmp') > 0) {
-      wx.showToast({
-        title: '请上传学生证',
-        icon: 'none'
-      })
-    }else if (this.data.stu_card == '' || this.data.stu_card.indexOf('tmp')>0) {
-      wx.showToast({
-        title: '请上传学生证',
-        icon: 'none'
-      })
-    }else {
-      wx.showLoading({
-        title: '加载中',
-      })
-      let url = 'regis'
-      if(this.data.res){
-        url = 'update/info'
-      }
-      app.com.post('wx/user/'+url, {
-        wx_id: wx.getStorageSync("user").id,
-        form_id: formId,
-        name: e.detail.value.name,
-        card_num: e.detail.value.card_num,
-        cert: _this.data.cert,
-        stu_card: _this.data.stu_card,
-        id: _this.data.res ? _this.data.res.id:'',
-        a_id: wx.getStorageSync("area").pk_id
-      }, function (res) {
-        wx.hideLoading()
-        if (res.code == 1) {
-          wx.showToast({
-            title: '提交成功',
-          })
-          if (_this.data.res) {
-            wx.setStorageSync("res", _this.data.res)
-          }
-          _this.setData({
-            'res.state': 0,
-            show: false,
-            isUpdate:false
-          })
-        } else {
-          wx.showToast({
-            title: res.msg,
-            icon: 'none'
-          })
-        }
-      })
-      
-    }
+  submit() {
+    console.log(this);
   },
   makePhone(){
     wx.makePhoneCall({
@@ -148,54 +100,12 @@ Page({
     })
   },
   onLoad: function (options) {
-    _this = this
-    let server = wx.getStorageSync("server")
-    for(let i in server){
-      server[i].pk_server = ((server[i].dl_sy + server[i].p_sy)*100).toFixed(0)
-    }
-    this.setData({
-      server: server
-    })
+
   },
   onShow(){
-      this.getRes()
+     
     
-  },
-  getRes() {
-    wx.showLoading({
-      title: '加载中',
-      task:true
-    })
-    app.com.post('wx/user/get/info/wxid', { wx_id: wx.getStorageSync("user").id }, function (res) {
-      wx.hideLoading()
-      if (res.code == 1) {
-        if(res.data != 0){
-          wx.setStorageSync("res", res.data)
-          _this.setData({
-            res: res.data,
-            show: false,
-            cert: res.data.cert,
-            stu_card: res.data.stu_card,
-            name: res.data.name,
-            card_num: res.data.card_num,
-          })
-        }else{
-          if (wx.getStorageSync("xy")) {
-
-          } else {
-            wx.navigateTo({
-              url: '/pages/mine/jdsm/jdsm',
-            })
-          }
-        }
-      } else {
-        wx.showToast({
-          title: '失败',
-          icon: 'none'
-        })
-      }
-    })
-  },
-
+  }
+ 
 
 })
